efb325b759062ff6dfacd8e3a6f21610267d9ed4
Sal Calabrese <erotavlas103@gmail.com>
2019-03-20 10:17:18 -0400

    add ConstructorInjected test

---
+++ src/test/java/guru/springframework/controller/ConstructorInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guru.springframework.services.GreetingServiceImpl;
+
+public class ConstructorInjectedControllerTest {
+	
+	private ConstructorInjectedController constructorInjectedController;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+	}
+
+	@Test
+	public void test() throws Exception {
+		assertEquals(GreetingServiceImpl.HELLO_GURUS, constructorInjectedController.sayHello());
+	}
+
+}
