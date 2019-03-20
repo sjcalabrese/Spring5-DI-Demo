7cbc350d9995aebdd2f6a4229e4ed48fa2c3523e
Sal Calabrese <erotavlas103@gmail.com>
2019-03-20 11:28:32 -0400

    add primary annotation and refactor myController

--- src/main/java/guru/springframework/controller/MyController.java
+++ src/main/java/guru/springframework/controller/MyController.java
@@ -2,13 +2,23 @@ package guru.springframework.controller;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.services.GreetingService;
+
 @Controller
 public class MyController {
 	
+	private GreetingService greetingService;
+	
+	
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+
+
 	public String hello() {
 		System.out.println("hello");
 		
-		return "foo";
+		return greetingService.sayGreeting();
 	}
 
 }
