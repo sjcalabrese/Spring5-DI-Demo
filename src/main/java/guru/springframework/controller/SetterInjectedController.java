424fe37968c179e5903188d2d59d6b9c8ac5186e
Sal Calabrese <erotavlas103@gmail.com>
2019-03-20 09:57:02 -0400

    Add test cases

--- src/main/java/guru/springframework/controller/SetterInjectedController.java
+++ src/main/java/guru/springframework/controller/SetterInjectedController.java
@@ -1,5 +1,17 @@
 package guru.springframework.controller;
 
+import guru.springframework.services.GreetingService;
+
 public class SetterInjectedController {
+	
+	private GreetingService greetingService;
+	
+	String sayHello() {
+		return greetingService.sayGreeting();
+	}
+	
+	public void getGreetingService(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
 
 }
--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -1,5 +1,13 @@
 package guru.springframework.services;
 
-public class GreetingServiceImpl {
+public class GreetingServiceImpl implements GreetingService {
+	
+	public static final String HELLO_GURUS = "Hello Gurus!!!";
+	
+	@Override
+	public String sayGreeting() {
+		// TODO Auto-generated method stub
+		return HELLO_GURUS;
+	}
 
 }
