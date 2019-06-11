f36ea2f9893e36b74b9ee7cfd3b25f56e9473ff9
Sal Calabrese <erotavlas103@gmail.com>
2019-06-11 11:32:59 -0400

    Migrated into intellij.

--- src/main/java/guru/springframework/services/ConstructorGreetingService.java
+++ src/main/java/guru/springframework/services/ConstructorGreetingService.java
@@ -1,8 +1,10 @@
 package guru.springframework.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class ConstructorGreetingService implements GreetingService {
 	
 	@Override
