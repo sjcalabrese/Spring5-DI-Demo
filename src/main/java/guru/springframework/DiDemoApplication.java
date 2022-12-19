a551b624316b47d4b3ab1192a43745cc244a99a7
Sal Calabrese <erotavlas103@gmail.com>
2022-12-19 14:14:15 -0500

    adding profiles

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework;
 
-import guru.springframework.controllers.ConstructorInjectedController;
-import guru.springframework.controllers.GetterInjectedController;
-import guru.springframework.controllers.MyController;
-import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.controllers.*;
 import guru.springframework.datasource.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,12 +11,13 @@ public class DiDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
-
 		//when creating a bean it will create the bean with the class name but with lower case first letter
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println("-----primary bean");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
+		System.out.println("-----primary bean");
 		System.out.println(controller.hello());
 		System.out.println("-------property");
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
